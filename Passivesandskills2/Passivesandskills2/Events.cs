44301f6e5f0b227a5a9d87804e92d91c55caf3fd
albertinchu Martin Palomino <albertinchu@hotmail.com>
2019-09-17 19:48:49 +0200

    En proceso

--- Passivesandskills2/Passivesandskills2/Events.cs
+++ Passivesandskills2/Passivesandskills2/Events.cs
@@ -24,14 +24,13 @@ namespace Passivesandskills2
         //Variables importantes y diccionarios//
         static Dictionary<string, bool> Scp173 = new Dictionary<string, bool>();
         static Dictionary<string, Vector> Scp173pos = new Dictionary<string, Vector>();
-        static Dictionary<string, bool> Classdh = new Dictionary<string, bool>();
-        static Dictionary<string, bool> Scientisth = new Dictionary<string, bool>();
+        
        
         
-        static Dictionary<string, int> Zombie = new Dictionary<string, int>();
+        
         
         static Dictionary<string, bool> Computerr = new Dictionary<string, bool>();
-        static Dictionary<string, Player> Pasiva = new Dictionary<string, Player>();
+      
         bool Nuket = false;
         bool Boom = false;
         bool Llorona = false;
@@ -56,20 +55,7 @@ namespace Passivesandskills2
                 computerchan = ev.Player.SteamId;
                 Computerr.Add(ev.Player.SteamId, true);
             }
-            if ((ev.Player.TeamRole.Role == Role.SCP_049_2))
-            {
-                if (!Zombie.ContainsKey(ev.Player.SteamId))
-                {
-                    Zombie.Add(ev.Player.SteamId, 0);
-                    Timing.Run(Zombielive(ev.Player));
-                }
-                ev.Player.PersonalBroadcast(10, "Tu pasiva es [Cuerpo errante]: Cuanto mas tiempo permanezcas con vida mas daño haces (15% + de daño cada 1 minuto de vida).", false);
-            }
-            if (ev.Player.TeamRole.Role == Role.SCP_049)
-            {
-                ev.Player.SendConsoleMessage("[Mutar]: Cada 6 Zombies curados el zombie número 6 tiene un 35 % de mutar en otro SCP a los 3 minutos, No puede mutar en SCP-096 o en SCP-079, La mutación es totalmente aleatoria ", "red");
-                ev.Player.PersonalBroadcast(10, "Tu pasiva es [Manipulador de cuerpos]: Curas de forma instantanea a clasesd/scientists [Mutar]: Cada 6 zombies uno tiene posibilidades de mutar (mas info en la consola)  .", false);
-            }
+            
             
             
             
@@ -90,55 +76,7 @@ namespace Passivesandskills2
         }
  
       
-        public static IEnumerable<float> Zombielive(Player player)
-        {
-            while (player.TeamRole.Role == Role.SCP_049_2)
-            {
-                yield return 60f;
-                Zombie[player.SteamId] += 1;
-            }
-            if (player.TeamRole.Role != Role.SCP_049_2)
-            {
-                Zombie[player.SteamId] = 0;
-            }
-        }
-        public static IEnumerable<float> Resurrec(Player player, Vector posdead)
-        {
-            yield return 3f;
-            player.ChangeRole(Role.SCP_049_2);
-            yield return 0.2f;
-            player.Teleport(posdead);
-        }
-
-
-
-        public static IEnumerable<float> Mutar(Player player)
-        {
-            System.Random proba = new Random();
-            int numero = proba.Next(0, 100);
-            yield return 5f;
-
-            if ((numero >= 5))
-            { player.ChangeRole(Role.SCP_106, false); }
-            if ((numero >= 10) && (numero <= 6))
-            { player.ChangeRole(Role.SCP_049, false); }
-            if ((numero >= 16) && (numero <= 20))
-            { player.ChangeRole(Role.SCP_049, false); }
-            if ((numero >= 95) && (numero <= 100))
-            { player.ChangeRole(Role.SCP_173, false); }
-            if (((numero >= 89) && (numero <= 94)))
-            {
-                if (numero < 92)
-                {
-                    player.ChangeRole(Role.SCP_939_53, false);
-                }
-                else
-                {
-                    player.ChangeRole(Role.SCP_939_89, false);
-                }
-            }
-
-        }
+   
        
         public static IEnumerable<float> Scp173timer(Player player, Vector pos)
         {
@@ -166,15 +104,7 @@ namespace Passivesandskills2
 
             }
         }
-        public static IEnumerable<float> ClassdTimer(Player player)
-        {
-            player.SetGhostMode(true, false, false);
-            yield return 10f;
-            player.SetGhostMode(false);
-            yield return 50f;
-            player.GiveItem(ItemType.FLASHLIGHT);
-            Classdh[player.SteamId] = true;
-        }
+     
        
         
        
@@ -220,10 +150,7 @@ namespace Passivesandskills2
            
 
             //Zombie//
-            if (ev.Attacker.TeamRole.Role == Role.SCP_049_2)
-            {
-                ev.Damage += (ev.Damage / 100) * 15 * Zombie[ev.Attacker.SteamId];
-            }
+          
             // CADETES //
         
             
@@ -268,8 +195,7 @@ namespace Passivesandskills2
 
         public void OnMedkitUse(PlayerMedkitUseEvent ev)
         {
-            if (ev.Player.TeamRole.Role == Role.CLASSD) { Pasiva[ev.Player.SteamId].GiveItem(ItemType.CHAOS_INSURGENCY_DEVICE); } else { Pasiva[ev.Player.SteamId].GiveItem(ItemType.FRAG_GRENADE); }
-          
+            
         }
 
         public void OnThrowGrenade(PlayerThrowGrenadeEvent ev)
@@ -319,12 +245,11 @@ namespace Passivesandskills2
             
             Scp173.Clear();
             Scp173pos.Clear();
-            Classdh.Clear();
-            Scientisth.Clear();
             
             
-            Zombie.Clear();
-            Pasiva.Clear();
+            
+            
+           
 
 
             Nuket = false;
@@ -354,7 +279,7 @@ namespace Passivesandskills2
 
             }
         }
-
+        static Dictionary<string, Player> Pasiva = new Dictionary<string, Player>();
         public void OnPlayerDie(PlayerDeathEvent ev)
         {
             //173 //
@@ -398,22 +323,7 @@ namespace Passivesandskills2
             }
 
             //doctor//
-            if (ev.Killer.TeamRole.Role == Smod2.API.Role.SCP_049)
-            {
-
-                posmuertee = ev.Player.GetPosition();
-                if ((ev.Player.TeamRole.Team == Team.SCIENTIST) || (ev.Player.TeamRole.Team == Team.CLASSD))
-                {
-                    Timing.Run(Resurrec(ev.Player, posmuertee));
-                    conta049 += 1;
-                    if (conta049 == 6)
-                    {
-                        Timing.Run(Mutar(ev.Player));
-                        conta049 = 0;
-                    }
-                }
-
-            }
+            
             //////////////////////////////////////////////////////ordenador/////////////////////////////////////////////////////
             if (overcharge == false)
             {
